52e8ae4965c0e0ef9bfbc24edc3b5899a76ef43a
xicheng85 <Administrator@Win10-E5-2660v2>
2020-02-20 22:36:32 +0800

    wangsu   lai dian suduba

--- GitText/src/com/chris/service/TextService.java
+++ GitText/src/com/chris/service/TextService.java
@@ -8,5 +8,6 @@ public class TextService {
 	public static void main(String[] args) {
 			 System.out.println("²âÊÔÊä³ö¸üĞÂ");
 			 new Text2().m2();
+			 System.out.println("zhe wangsu   yundao ");
 	}
 }
