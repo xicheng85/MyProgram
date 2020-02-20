eb3eb0968f70b3a1f7127a271cdc4e1c8848e3ff
xicheng85 <Administrator@Win10-E5-2660v2>
2020-02-20 23:47:24 +0800

    up  chongtu

--- GitText/src/com/chris/service/TextService.java
+++ GitText/src/com/chris/service/TextService.java
@@ -11,5 +11,6 @@ public class TextService {
 			 System.out.println("zhe wangsu   yundao ");
 			 System.out.println("这是E3 1230写的代码");
 			 System.out.println("我是分支2才有的代码");
+			 System.out.println("冲突演示");
 	}
 }
