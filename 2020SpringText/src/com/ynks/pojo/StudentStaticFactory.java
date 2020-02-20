f1f292fc8f1bcbb15f2dc72c84b2f7c215b28541
xicheng85 <Administrator@Win10-E5-2660v2>
2020-02-21 00:22:47 +0800

    dd

--- 2020SpringText/src/com/ynks/pojo/StudentStaticFactory.java
+++
@@ -1,7 +0,0 @@
-package com.ynks.pojo;
-
-public class StudentStaticFactory {
-		public static Student  newInstance(){
-			  return new Student();
-		}
-}
--- 2020SpringText/src/com/ynks/text/DemoT.java
+++
@@ -1,7 +0,0 @@
-package com.ynks.text;
-
-public class DemoT {
-		 public void dO() {
-			   System.out.println("DO");
-		}
-}
